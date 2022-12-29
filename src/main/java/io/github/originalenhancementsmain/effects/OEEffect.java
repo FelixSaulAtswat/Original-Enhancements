edb4cedf05c78221d251c0772dcc9c68c3a9ba40
lornlotus <lwowvhe3498@163.com>
2022-12-29 14:01:06 +0800

    111

--- src/main/java/io/github/originalenhancementsmain/effects/OEEffect.java
+++ src/main/java/io/github/originalenhancementsmain/effects/OEEffect.java
@@ -14,7 +14,7 @@ public class OEEffect {
     public static final DeferredRegister<MobEffect> EFFECTS = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, OriginalEnhancementsMain.MOD_ID);
 
     public static final RegistryObject<MobEffect> FROSTMOURNE_EFFECT = EFFECTS.register("frostmourne_effect",()->
-            new FrostmourneEffect(MobEffectCategory.HARMFUL, 2883584,false).addAttributeModifier(Attributes.MOVEMENT_SPEED, "7107DE5E-7CE8-4030-940E-514C1F160890", -0.5F, AttributeModifier.Operation.MULTIPLY_TOTAL));
+            new FrostmourneEffect(MobEffectCategory.HARMFUL, 60159,false).addAttributeModifier(Attributes.MOVEMENT_SPEED, "7107DE5E-7CE8-4030-940E-514C1F160890", -0.5F, AttributeModifier.Operation.MULTIPLY_TOTAL));
 
 }
 
