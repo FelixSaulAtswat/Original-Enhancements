80488f480a8415c0226bbe68800821186ed7db2a
lornlotus <lwowvhe3498@163.com>
2022-12-31 17:37:59 +0800

    111

---
+++ src/main/java/io/github/originalenhancementsmain/oeblock/apparatusblock/ApparatusBlock.java
@@ -0,0 +1,34 @@
+package io.github.originalenhancementsmain.oeblock.apparatusblock;
+
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+
+public abstract class ApparatusBlock extends BaseApparatusBlock{
+
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+    public static final BooleanProperty ACTIVE = BooleanProperty.create("active");
+
+    protected ApparatusBlock(Properties properties){
+        super(properties);
+
+        this.registerDefaultState(this.defaultBlockState().setValue(ACTIVE, false));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING, ACTIVE);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context){
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+
+
+}
---
+++ src/main/java/io/github/originalenhancementsmain/oeblock/apparatusblock/BaseOEMenu.java
@@ -0,0 +1,62 @@
+package io.github.originalenhancementsmain.oeblock.apparatusblock;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+
+public class BaseOEMenu <T extends BlockEntity> extends AbstractContainerMenu {
+
+    private final T oeb;
+    private final Inventory inventory;
+    protected BaseOEMenu(int containerId , MenuType<?> menuType, Inventory inventory, T oeb){
+        super(menuType, containerId);
+        this.oeb = oeb;
+        this.inventory = inventory;
+    }
+
+    @Override
+    public boolean stillValid(Player player) {
+
+
+        if (this.oeb == null){
+            return true;
+        }
+        if (!oeb.isRemoved()){
+            Level level = oeb.getLevel();
+            if (level == null){
+                return false;
+            }
+            return level.isLoaded(oeb.getBlockPos());
+        }
+        return false;
+    }
+
+    public void openNatureApparatusMenu(ServerPlayer serverPlayer) {
+        ServerLevel level = serverPlayer.getLevel();
+        for (Player player : level.players())
+            if (serverPlayer == player){
+                if (player.containerMenu instanceof BaseOEMenu){
+                    if (this.giveGui((BaseOEMenu) player.containerMenu)){
+                        this.giveOtherContainer((BaseOEMenu) player.containerMenu, serverPlayer);
+                        return;
+                    }
+                }
+            }
+        this.giveNewContainer(serverPlayer);
+    }
+
+    public boolean giveGui(BaseOEMenu menu){
+        if (this.oeb == null){
+            return false;
+        }
+        return this.oeb == menu.oeb;
+    }
+
+    protected void giveOtherContainer(BaseOEMenu menuContainer, ServerPlayer player){}
+    protected void giveNewContainer(ServerPlayer player){}
+}
